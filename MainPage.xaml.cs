6a77c336397bfd3de16a1665c7d0a093de613bc1
Silvano Stecher <silvano.stecher@gmail.com>
2024-08-12 21:04:56 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EChargeFinder
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ModelJSON.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EChargeFinder
+{
+    public class Address
+    {
+        public string HouseNum { get; set; }
+        public string TimeZone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+    }
+
+    public class ChargingFacility
+    {
+        public int Power { get; set; }
+        public string Powertype { get; set; }
+    }
+
+    public class ChargingStationName
+    {
+        public string Lang { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class GeoCoordinates
+    {
+        public string Google { get; set; }
+    }
+
+    public class EVSEDataRecord
+    {
+        public string AccessibilityLocation { get; set; }
+        public Address Address { get; set; }
+        public List<string> AuthenticationModes { get; set; }
+        public string CalibrationLawDataAvailability { get; set; }
+        public List<ChargingFacility> ChargingFacilities { get; set; }
+        public List<ChargingStationName> ChargingStationNames { get; set; }
+        public string DynamicInfoAvailable { get; set; }
+        public string HotlinePhoneNumber { get; set; }
+        public string HubOperatorID { get; set; }
+        public bool IsHubjectCompatible { get; set; }
+        public bool IsOpen24Hours { get; set; }
+        public List<string> PaymentOptions { get; set; }
+        public List<string> Plugs { get; set; }
+        public bool RenewableEnergy { get; set; }
+        public string Accessibility { get; set; }
+        public string ChargingStationId { get; set; }
+        public GeoCoordinates GeoCoordinates { get; set; }
+        public string LastUpdate { get; set; }
+        public string EvseID { get; set; }
+    }
+
+    public class EVSEData
+    {
+        public List<EVSEDataRecord> EVSEDataRecord { get; set; }
+    }
+
+    public class Root
+    {
+        public List<EVSEData> EVSEData { get; set; }
+    }
+}
