abfc09d89bd3c9e05b01cfcb146d20f89eca9403
Silvano Stecher <silvano.stecher@gmail.com>
2024-08-15 18:03:32 +0200

    some fixes of the code

--- Core/JSONCleanUp.cs
+++ Core/JSONCleanUp.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 
 namespace EChargeFinder.Core
 {
@@ -8,44 +7,45 @@ namespace EChargeFinder.Core
         // this function fixes the ChargingStationNames inconsistencies which are coming from the API
         public string cleanUpChargingStationAPINamingError(string jsonString)
         {
-            JObject jsonObject = JObject.Parse(jsonString);
+            var jsonObject = JObject.Parse(jsonString);
 
-            // Process each EVSEData record
             foreach (var evseData in jsonObject["EVSEData"])
             {
                 foreach (var record in evseData["EVSEDataRecord"])
                 {
-                    // Check if ChargingStationNames is an object
                     if (record["ChargingStationNames"].Type == JTokenType.Object)
                     {
-                        var singleObject = record["ChargingStationNames"] as JObject;
+                        var singleObject = (JObject)record["ChargingStationNames"];
                         var lang = singleObject["lang"]?.ToString();
                         var value = singleObject["value"]?.ToString();
 
-                        // Replace with an array containing the single object
-                        record["ChargingStationNames"] = new JArray(
-                            new JObject
-                                    {
-                            { "lang", lang },
-                            { "value", value }
-                                    }
-                        );
+                        record["ChargingStationNames"] = new JArray
+                {
+                    new JObject
+                    {
+                        { "lang", lang },
+                        { "value", value }
+                    }
+                };
                     }
                     else if (record["ChargingStationNames"].Type == JTokenType.Array)
                     {
-                        // Ensure that each item in the array is of the correct format
-                        var array = record["ChargingStationNames"] as JArray;
+                        var array = (JArray)record["ChargingStationNames"];
                         foreach (var item in array)
                         {
-                            if (item["lang"] == null || item["value"] == null)
+                            if (item.Type == JTokenType.Object)
                             {
-                                // Handle invalid items if necessary
+                                var itemObject = (JObject)item;
+                                if (itemObject["lang"] == null || itemObject["value"] == null)
+                                {
+                                    // Handle invalid items if necessary
+                                }
                             }
                         }
                     }
                 }
             }
-            return jsonObject.ToString(Formatting.Indented);
+            return jsonObject.ToString(Newtonsoft.Json.Formatting.Indented);
         }
     }
 }
